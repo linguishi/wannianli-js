fd04475ad7876fcb636b38d214d8fe00378a2809
linguishi <513228837@qq.com>
2019-09-26 15:43:18 +0800

    add README

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.get('/getDateInfo', function (req, res) {
 });
 
 var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("host: ", host);
+    console.log("port: ", port);
 });
